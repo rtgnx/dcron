45ed90c1de2c6784753633c14a1f16e3466e9e39
Adrian Cybulski <adrian@cybulski.cc>
2021-02-13 09:36:08 +0000

    Wait for container by selecting channel

--- runner.go
+++ runner.go
@@ -62,9 +62,16 @@ func (r Runner) startContainer(id string) error {
 	return c.ContainerStart(context.Background(), id, types.ContainerStartOptions{})
 }
 
-func (r Runner) containerWait(id string) {
+func (r Runner) containerWait(id string) error {
 	c := client.Client(r)
-	c.ContainerWait(context.Background(), id, container.WaitConditionNextExit)
+	okCh, errCh := c.ContainerWait(context.Background(), id, container.WaitConditionNextExit)
+
+	select {
+	case <-okCh:
+		return nil
+	case err := <-errCh:
+		return err
+	}
 }
 
 func (r Runner) removeContainer(id string, force bool) error {
